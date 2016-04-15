cf61e5e0100cebaf5ecef7269e8988021fb2d555
Abhishek Srivastava <abhishek.srivastava@dealertrack.com>
2016-04-15 00:48:48 -0500

    added maven docker plugin support

--- src/main/scala/HelloWorld.scala
+++ src/main/scala/HelloWorld.scala
@@ -1,7 +1,23 @@
  package com.abhi
 
+ import org.apache.spark.SparkConf
+ import org.apache.spark.SparkContext
+ import org.apache.spark.rdd.RDD
+ import org.apache.spark.sql._
+ import org.apache.spark.sql.types.{StringType, StructField, StructType}
+
  object HelloWorld {
    def main(args: Array[String]) : Unit = {
-     println("Hello World")
+     val conf = new SparkConf().setAppName("IntegrationTestSparkJob")
+     val sc = new SparkContext(conf)
+     val sqlSc = new SQLContext(sc)
+     val Array(inputPath, outputPath) = args
+     // read the input file from input param 1
+     val df = sqlSc.load(inputPath, "com.databricks.spark.avro")
+     // write the count to input param2
+     val schema = StructType(StructField("count", StringType, false) :: Nil)
+     val outRdd = sc.parallelize(List(df.rdd.count().toString).map(c => Row(c)))
+     val outDf = sqlSc.createDataFrame(outRdd, schema)
+     outDf.save(outputPath, "com.databricks.spark.avro")
    }
  }
